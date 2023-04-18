88e51cc519d4174498f7563924cecad747fc169b
Akimova_Anna <miss.anna1103@gmail.com>
2023-04-18 14:02:12 +0300

    New directories and window with icon and project name

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((600,300)) #размеры экрана через кортеж (ширина,высота)
+pygame.display.set_caption("Jumping_Luna")
+icon = pygame.image.load('images/icon.png')
+pygame.display.set_icon(icon)
+
+running = True
+while running: #запуск бесконечного цикла игры до нажатия кнопки закрыть окно
+
+    pygame.display.update()
+
+    for event in pygame.event.get(): #перебор всех событий в окне
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
 
