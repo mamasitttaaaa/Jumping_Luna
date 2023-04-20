b44567ca1beb521e26d150f29ed1a0cf20eaaaba
Akimova_Anna <miss.anna1103@gmail.com>
2023-04-20 13:49:37 +0300

    New background image and sound

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ icon = pygame.image.load('images/icon.png')
 pygame.display.set_icon(icon)
 
 bg = pygame.image.load('images/background1.jpg')
-player = pygame.image.load('images/left_moves/left_move1.png')
+
 move_left = [
     pygame.image.load('images/left_moves/left_move1.png'),
     pygame.image.load('images/left_moves/left_move2.png'),
@@ -20,35 +20,56 @@ move_right = [
     pygame.image.load('images/right_moves/right_move2.png'),
     pygame.image.load('images/right_moves/right_move3.png')
 ]
-player_anim_counter = 0
-bg_x = 0
+hero_anim_counter = 0 #счетчик анимаций для цикла, чтобы показывать все картинки из списка беспрерывно
+bg_x = 0 #для смещения фона (две одинаковых картинки идут друг за другом)
+#параметры положения игрока и его движения
+hero_speed = 5
+hero_x = 300
+hero_y = 200
 bg_sound = pygame.mixer.Sound('sounds/background.mp3')
 bg_sound.play()
 
 running = True
 while running: #запуск бесконечного цикла игры до нажатия кнопки закрыть окно
 
-
-    screen.blit(bg, (bg_x,0))
-    screen.blit(bg, (bg_x + 600, 0))
-    screen.blit(move_right[player_anim_counter], (300, 200))
-
-    if player_anim_counter == 2:
-        player_anim_counter = 0
-    else:
-        player_anim_counter += 1
-
-
-    bg_x -= 2
-    if bg_x == -618:
-        bg_x = 0
     pygame.display.update()
 
     for event in pygame.event.get(): #перебор всех событий в окне
         if event.type == pygame.QUIT:
             running = False
-            pygame.quit()
+            #выводит ошибку display Surface quit
+            # pygame.quit()
+            #безошибочный вариант
+            break
 
-    clock.tick(10)
+    screen.blit(bg, (bg_x, 0))
+    screen.blit(bg, (bg_x + 600, 0))
+
+    #переменная, содержащая действия пользователя
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        screen.blit(move_left[hero_anim_counter], (hero_x, hero_y))
+    else:
+        screen.blit(move_right[hero_anim_counter], (hero_x, hero_y))
+
+    # отслживание действий пользователя, чтобы изменять положение героя
+    if keys[pygame.K_LEFT] and hero_x > 50:
+        hero_x -= hero_speed
+    elif keys[pygame.K_RIGHT] and hero_x < 400:
+        hero_x += hero_speed
+
+    if hero_anim_counter == 2:
+        hero_anim_counter = 0
+    else:
+        hero_anim_counter += 1
+
+
+    bg_x -= 2 #отвечает за сдвиг фона по координатам при каждой итерации цикла
+    if bg_x == -600:
+        bg_x = 0
+    # pygame.display.update()
+
+    clock.tick(10) #задержка перед новой итерацией цикла (медленное переключение анимаций персонажа и медленная прокрутка фона)
 
 
